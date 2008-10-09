d3ba0a9cf3599cb1c87eefce7b9df115e9bd69f5
Duncan Beevers <duncanbeevers@gmail.com>
2008-10-09 00:57:43 -0700

    Create chainier syntax for form building in order to avoid repeating field names.

--- lib/in_place_form_builder.rb
+++ lib/in_place_form_builder.rb
@@ -1,4 +1,34 @@
 module InPlaceForm
+  class FieldContext
+    attr_reader :field_name
+    
+    def initialize field_name, form_builder
+      @field_name = field_name
+      @form_builder = form_builder
+    end
+    
+    def static static_content_generator = nil
+      # simply forward receiver on as result of method call if no static work to be done
+      return self unless @form_builder.static?
+      InPlaceForm::StaticContentProxy.new(static_content(static_content_generator), self)
+    end
+    
+    def static_content static_content_generator = nil
+      static_content_generator ||= lambda { |object, field_name| object.send(field_name) }
+      case static_content_generator
+        when String
+          static_content_generator
+        when Proc
+          static_content_generator.call(@form_builder.object, field_name)
+      end
+    end
+    
+    def method_missing method_name, *args
+      return static_content if @form_builder.static?
+      @form_builder.send method_name, field_name, *args
+    end
+  end
+  
   class StaticContentProxy
     def initialize static_content, form_builder
       @static_content = static_content
@@ -13,10 +43,16 @@ end
 
 class InPlaceFormBuilder < ActionView::Helpers::FormBuilder
   def initialize object_name, object, template, options, proc
-    @static = options.delete(:static)
+    @static = options.delete :static
     super
   end
-  def static static_content
-    @static ?InPlaceForm::StaticContentProxy.new(static_content, self) :  self
+  
+  def static?
+    @static
   end
+  
+  def field field_name
+    InPlaceForm::FieldContext.new field_name, self
+  end
+  
 end
--- test/renderer_controller_test.rb
+++ test/renderer_controller_test.rb
@@ -13,10 +13,10 @@ class RendererControllerTest < ActionController::TestCase
     get :render_template,
       :eval => '@post = Post.new',
       :template => <<-END_TEMPLATE
-<%- form_for @post, :builder => InPlaceFormBuilder do |f| -%>
-<h1><%= f.static('static content').text_field(:title) -%></h1>
-<%- end -%>
-END_TEMPLATE
+        <%- form_for @post, :builder => InPlaceFormBuilder do |f| -%>
+        <h1><%= f.field(:title).static('static content').text_field -%></h1>
+        <%- end -%>
+        END_TEMPLATE
     assert_select 'h1>input[type=text]'
   end
 
@@ -24,10 +24,43 @@ END_TEMPLATE
     get :render_template,
       :eval => '@post = Post.new',
       :template => <<-END_TEMPLATE
-<%- form_for @post, :builder => InPlaceFormBuilder, :static => true do |f| -%>
-<h1><%= f.static('static content').text_field(:title) -%></h1>
-<%- end -%>
-END_TEMPLATE
+        <%- form_for @post, :builder => InPlaceFormBuilder, :static => true do |f| -%>
+        <h1><%= f.field(:title).static('static content').text_field -%></h1>
+        <%- end -%>
+        END_TEMPLATE
     assert_select 'h1', 'static content'
+  end
+  
+  def test_static_is_optional
+    get :render_template,
+      :eval => '@post = Post.new',
+      :template => <<-END_TEMPLATE
+        <%- form_for @post, :builder => InPlaceFormBuilder do |f| -%>
+        <h1><%= f.field(:title).text_field -%></h1>
+        <%- end -%>
+        END_TEMPLATE
+    assert_select 'h1>input[type=text]'
+  end
+  
+  def test_static_should_render_field_value
+    get :render_template,
+      :eval => '@post = Post.new(:title => "post title")',
+      :template => <<-END_TEMPLATE
+        <%- form_for @post, :builder => InPlaceFormBuilder, :static => true do |f| -%>
+        <h1><%= f.field(:title).text_field -%></h1>
+        <%- end -%>
+        END_TEMPLATE
+    assert_select 'h1', 'post title'
+  end
+  
+  def test_should_accept_field_options
+    get :render_template,
+      :eval => '@post = Post.new',
+      :template => <<-END_TEMPLATE
+        <%- form_for @post, :builder => InPlaceFormBuilder do |f| -%>
+        <h1><%= f.field(:title).text_field(:class => :mandatory) -%></h1>
+        <%- end -%>
+        END_TEMPLATE
+    assert_select 'h1>input[class=mandatory]'
   end
 end
